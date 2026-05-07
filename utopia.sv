fb7f046457a3439c91770b1d770d4385fcff7205
Asim <261854487+asim-asic@users.noreply.github.com>
2026-05-07 09:27:19 +0530

    Add utopia interface

---
+++ utopia.sv
@@ -0,0 +1,40 @@
+interface utopia;
+  parameter int Ifwidth = 8;
+
+  bit clk_in;
+  bit clk_out;
+  logic [Ifwidth-1:0] data;
+  bit soc;
+  bit clav;
+  bit valid;
+  bit ready;
+  bit reset;
+  bit selected;
+
+  ATMCellType ATMcell;  // union of structures for ATM cells
+
+  modport TopReceive(
+      input clk_in, data, soc, clav, ready, reset,
+      output clk_out, en, ATMcell, valid
+  );
+
+
+  modport TopTransmit(
+      input clk_in, clav, ATMcell, ready, reset,
+      output clk_out, data, soc, en, valid
+  );
+
+  modport CoreReceive(
+      input clk_in, data, soc, clav, ready, reset,
+      output clk_out, en, ATMcell, valid
+  );
+
+  modport CoreTransmit(
+      input clk_in, clav, ATMcell, valid, reset,
+      output clk_out, data, soc, en, ready
+  );
+
+`ifndef SYNTHESIS  // synthesis ignores this code
+  UtopiaMethod Method;  // interface with testing methods
+`endif
+endinterface
