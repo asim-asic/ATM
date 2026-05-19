873ac8fb15cd91a561b1602b77a24ac3c5c4bec1
Asim <261854487+asim-asic@users.noreply.github.com>
2026-05-20 00:11:21 +0530

    Update and add top.sv

---
+++ top.sv
@@ -0,0 +1,11 @@
+/*`include "definitions.sv"
+`include "utopia.sv"
+`include "methods.sv"
+`include "squat.sv"
+`include "utopial_atm_rx.sv"
+`include "utopial_atm_tx.sv"
+`include "test.sv"*/
+
+module top;
+	test t();
+endmodule
